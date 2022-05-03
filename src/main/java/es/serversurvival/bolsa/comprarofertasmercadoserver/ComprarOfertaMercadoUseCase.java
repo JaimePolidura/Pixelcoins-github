3963115ce3abe00099b969ea6243758609690b3e
JaimeTruman <jaime.polidura@gmail.com>
2022-05-03 23:40:05 +0200

    updated comprarofertatmercado use case

--- src/main/java/es/serversurvival/bolsa/comprarofertasmercadoserver/ComprarOfertaMercadoUseCase.java
+++ src/main/java/es/serversurvival/bolsa/comprarofertasmercadoserver/ComprarOfertaMercadoUseCase.java
@@ -1,24 +1,52 @@
 package es.serversurvival.bolsa.comprarofertasmercadoserver;
 
 import es.serversurvival.Pixelcoin;
+import es.serversurvival._shared.DependecyContainer;
+import es.serversurvival._shared.utils.Funciones;
 import es.serversurvival.bolsa._shared.llamadasapi.mysql.TipoActivo;
 import es.serversurvival.bolsa._shared.ofertasmercadoserver.mysql.OfertaMercadoServer;
+import es.serversurvival.bolsa._shared.ofertasmercadoserver.mysql.TipoOfertante;
 import es.serversurvival.bolsa._shared.posicionescerradas.mysql.TipoPosicion;
 import es.serversurvival._shared.mysql.AllMySQLTablesInstances;
+import es.serversurvival.empresas._shared.mysql.Empresa;
+import es.serversurvival.jugadores._shared.newformat.application.JugadoresService;
+import es.serversurvival.jugadores._shared.newformat.domain.Jugador;
 
 
 public final class ComprarOfertaMercadoUseCase implements AllMySQLTablesInstances {
     public static final ComprarOfertaMercadoUseCase INSTANCE = new ComprarOfertaMercadoUseCase();
+    private final JugadoresService jugadoresService;
 
-    private ComprarOfertaMercadoUseCase () {}
+    private ComprarOfertaMercadoUseCase () {
+        this.jugadoresService = DependecyContainer.get(JugadoresService.class);
+    }
 
-    public void comprarOfertaMercadoAccionServer (String player, int idOfeta, int cantidadAComprar) {
+    public void comprarOfertaMercadoAccionServer (String compradorName, int idOfeta, int cantidadAComprar) {
         OfertaMercadoServer oferta = ofertasMercadoServerMySQL.get(idOfeta);
         double precioTotalAPagar = oferta.getPrecio() * cantidadAComprar;
 
-        posicionesAbiertasMySQL.nuevaPosicion(player, TipoActivo.ACCIONES_SERVER, oferta.getEmpresa(), cantidadAComprar, oferta.getPrecio(), TipoPosicion.LARGO);
+        posicionesAbiertasMySQL.nuevaPosicion(compradorName, TipoActivo.ACCIONES_SERVER, oferta.getEmpresa(), cantidadAComprar, oferta.getPrecio(), TipoPosicion.LARGO);
         ofertasMercadoServerMySQL.setCantidadOBorrar(idOfeta, oferta.getCantidad() - cantidadAComprar);
 
-        Pixelcoin.publish(new EmpresaServerAccionCompradaEvento(player, precioTotalAPagar, cantidadAComprar, oferta, oferta.getEmpresa()));
+        Jugador comprador = jugadoresMySQL.getJugador(compradorName);
+
+        jugadoresService.save(comprador.decrementPixelcoinsBy(oferta.getPrecio()));
+
+        if(oferta.getTipo_ofertante() == TipoOfertante.JUGADOR){
+            Jugador vendedor = jugadoresMySQL.getJugador(oferta.getJugador());
+
+
+            double beneficiosPerdidas = (oferta.getPrecio() - oferta.getPrecio_apertura()) * cantidadAComprar;
+            double rentabilidad = Funciones.redondeoDecimales(Funciones.diferenciaPorcntual(oferta.getPrecio_apertura(), oferta.getPrecio()), 3);
+
+            if(beneficiosPerdidas >= 0)
+                jugadoresMySQL.setEstadisticas(vendedor.getNombre(), vendedor.getPixelcoins() + precioTotalAPagar, vendedor.getNVentas(),
+                        vendedor.getIngresos(), vendedor.getGastos() + beneficiosPerdidas);
+            else
+                jugadoresMySQL.setEstadisticas(vendedor.getNombre(), vendedor.getPixelcoins() + precioTotalAPagar, vendedor.getNVentas(),
+                        vendedor.getIngresos() + beneficiosPerdidas, vendedor.getGastos());
+        }
+
+        Pixelcoin.publish(new EmpresaServerAccionCompradaEvento(compradorName, precioTotalAPagar, cantidadAComprar, oferta, oferta.getEmpresa()));
     }
 }
